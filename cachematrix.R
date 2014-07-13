5a923430ce2627d15998f5b2ccfe4ea670d92258
Claudio <ctakamiya@gmail.com>
2014-07-13 11:59:17 -0300

    A few explanatory comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program follows the same structure of example code Caching the Mean of 
+## a Vector. But, instead caching the mean of vector, this function caches the 
+## inverse of a matrix
 
-## Write a short comment describing this function
+
+## As the same idea of makeVector, this function creates a special Matrix, which
+## is really a list containing a function to: 
+## 1 - set the value of the matrix
+## 2 - get the value of the matrix
+## 3 - set the value of the inverse of matrix
+## 4 - get the value of the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## It is the best practice to validating input parameters.
+        if (nrow(x) != ncol(x)) {
+                stop("The matrix must be square matrix.")
+        }
         s <- NULL
-        set <- function(y) {
+        set <- function(y) {             
                 x <<- y
                 s <<- NULL
         }
